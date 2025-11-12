b92bf153b94ea05b6b8e163e9ecdf49870b5fa02
武汉大学 <143717599+creeper-RedWHU@users.noreply.github.com>
2025-11-12 14:04:58 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-DASHSCOPE_API_KEY = "sk-58808fff4527464ea4460a4293d1e112"  # qwen API
-TAVILY_API_KEY = "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"  # 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
+DASHSCOPE_API_KEY = "sk-"  # qwen API
+TAVILY_API_KEY = "tvly-"  # 
 LANGSMITH_API_KEY = "YOUR_API_KEY_HERE"
 
 skin_key = "YOUR_SKIN_KEY_HERE"
